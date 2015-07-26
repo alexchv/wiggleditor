a03f65a731a7d8d1903d5cd043b9824c1c00727c
Aleksandr Cherednikov <aleksandr.cherednikov.tgn@gmail.com>
2015-07-27 00:22:44 +0300

    cors config

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Wiggleditor
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.action_dispatch.default_headers = {
-        'Access-Control-Allow-Origin'   => 'http://localhost:3000',
+        'Access-Control-Allow-Origin'   => '*',
         'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }
 
