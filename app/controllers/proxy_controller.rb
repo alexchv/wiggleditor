062ded0f42665813a3544d45db0cd1b8188e1e5d
Aleksandr Cherednikov <aleksandr.cherednikov.tgn@gmail.com>
2015-07-27 00:14:31 +0300

    debug logs

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -2,6 +2,9 @@ class ProxyController < ApplicationController
 
   def highlighter
     file = open params[:url]
+    puts '----'
+    puts file.inspect
+    puts '----'
     render :inline => file.read
   end
 
